6d17a8ad4c0be592fa912328a5e61ced78b76901
Benjamin De Coninck <ben.de.coninck@gmail.com>
2020-03-13 21:48:12 +0100

    TF pour odométrie et laser (pour quand ce $ù^*m$ d'Hokuyo aura décidé de fonctionner)

--- scripts/tetra_utility.py
+++ scripts/tetra_utility.py
@@ -12,14 +12,18 @@ import math
 mgmt_pub = rospy.Publisher('cmd_mgmt', Int16, queue_size=10)
 odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
 odom_broadcaster = tf.TransformBroadcaster()
+laser_broadcaster = tf.TransformBroadcaster()
 
 def compactOdom_callback(odom_data):
 
-    #since all odometry is 6DOF we'll need a quaternion created from yaw
+    #since all odometry is 6DOF we'll need a quaternion created from yaw, for odometry and laser
     odom_quat = tf.transformations.quaternion_from_euler(0, 0, math.radians(odom_data.ang_pos))
+    laser_quat = tf.transformations.quaternion_from_euler(0, 0, 0)
 
-    #first, we'll publish the transform over tf
+    #first, we'll publish the odom transform over tf
     odom_broadcaster.sendTransform((odom_data.x_pos, odom_data.y_pos, 0.), odom_quat, odom_data.stamp, "base_link", "odom")
+    #the laser tf is 20 cm +x and 5cm +z (to check)
+    laser_broadcaster.sendTransform((0.20, 0., 0.10), laser_quat, odom_data.stamp, "odom", "laser")
     
     # next, we'll publish the odometry message over ROS
     odom = Odometry()
