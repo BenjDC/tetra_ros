13448872ec1c8664fc89ca09e7711ade73002007
Benjamin <ben.de.coninck@gmail.com>
2020-07-27 14:21:20 +0200

    Arduino turret calibration values

---
+++ ressources/test_servo/test_dc_ino/test_dc_ino.ino
@@ -0,0 +1,117 @@
+
+#include <ros.h>
+#include <sensor_msgs/Joy.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+#define PAN_INIT 1330
+#define PAN_RANGE 600
+
+#define TILT_INIT 1250
+#define TILT_RANGE 200
+
+#define FIRE_INIT 1700
+#define FIRE_PUSH 1000
+
+
+
+
+ros::NodeHandle  nh;
+
+void joy_cb( const sensor_msgs::Joy& cmd_msg) {
+
+  char message[100];
+
+  //int motor_power = (int)((cmd_msg.axes[2])*150);
+  int motor_power = 0;
+
+
+  if (cmd_msg.axes[0] != 0)
+  {
+    int servo_tilt = (int)(SERVO_MAX - SERVO_MIN * (cmd_msg.axes[0] + 1) / 2);
+    sprintf(message, "tilt : %d\n", servo_tilt);
+    nh.loginfo(message);
+    
+    pwm.writeMicroseconds(0, PAN_INIT);
+  }
+  if (cmd_msg.axes[1] != 0)
+  {
+    int servo_pan = (int)(SERVO_MAX - SERVO_MIN * (cmd_msg.axes[1] + 1) / 2);
+    sprintf(message, "pan : %d\n", servo_pan);
+    nh.loginfo(message);
+    pwm.setPWM(1, 0, servo_pan);
+  }
+
+  if (cmd_msg.axes[4] != 0)
+  {
+    int servo_gun = (int)(SERVO_MAX - SERVO_MIN * (cmd_msg.axes[4] + 1) / 2);
+    sprintf(message, "gun : %d\n", servo_gun);
+    pwm.setPWM(2, 0, servo_gun);
+    nh.loginfo(message);
+  }
+  if (motor_power > 0)
+  {
+
+    //Motor A forward @ configured speed
+    digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+    digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+    analogWrite(3, motor_power);   //Spins the motor on Channel A at full speed
+
+    //Motor B backward @ half speed
+    digitalWrite(13, LOW);  //Establishes backward direction of Channel B
+    digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+    analogWrite(11, motor_power);    //Spins the motor on Channel B at half speed
+
+  }
+  else
+  {
+
+    digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+    digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+
+  }
+
+}
+
+ros::Subscriber<sensor_msgs::Joy> sub("joy", joy_cb);
+
+void setup() {
+
+  pwm.begin();
+
+  //Setup Channel A
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+
+  //Setup Channel B
+  pinMode(13, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
+
+
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  pwm.writeMicroseconds(0, PAN_INIT);
+  pwm.writeMicroseconds(1, TILT_INIT);
+  pwm.writeMicroseconds(2, FIRE_INIT);
+
+  delay(10);
+
+  nh.initNode();
+  nh.subscribe(sub);
+
+}
+
+void loop() {
+  nh.spinOnce();
+  delay(10);
+
+}
